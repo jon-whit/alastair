27859255e131bbefaafd9fd574bcbd4eff31952e
james-tigue <james.tigue@utah.edu>
2016-11-09 12:00:44 -0700

    Add files via upload

---
+++ Sim_GUI.m
@@ -0,0 +1,271 @@
+function varargout = Sim_GUI(varargin)
+% SIM_GUI MATLAB code for Sim_GUI.fig
+%      SIM_GUI, by itself, creates a new SIM_GUI or raises the existing
+%      singleton*.
+%
+%      H = SIM_GUI returns the handle to a new SIM_GUI or the handle to
+%      the existing singleton*.
+%
+%      SIM_GUI('CALLBACK',hObject,eventData,handles,...) calls the local
+%      function named CALLBACK in SIM_GUI.M with the given input arguments.
+%
+%      SIM_GUI('Property','Value',...) creates a new SIM_GUI or raises the
+%      existing singleton*.  Starting from the left, property value pairs are
+%      applied to the GUI before Sim_GUI_OpeningFcn gets called.  An
+%      unrecognized property name or invalid value makes property application
+%      stop.  All inputs are passed to Sim_GUI_OpeningFcn via varargin.
+%
+%      *See GUI Options on GUIDE's Tools menu.  Choose "GUI allows only one
+%      instance to run (singleton)".
+%
+% See also: GUIDE, GUIDATA, GUIHANDLES
+
+% Edit the above text to modify the response to help Sim_GUI
+
+% Last Modified by GUIDE v2.5 07-Nov-2016 19:09:40
+
+% Begin initialization code - DO NOT EDIT
+gui_Singleton = 1;
+gui_State = struct('gui_Name',       mfilename, ...
+                   'gui_Singleton',  gui_Singleton, ...
+                   'gui_OpeningFcn', @Sim_GUI_OpeningFcn, ...
+                   'gui_OutputFcn',  @Sim_GUI_OutputFcn, ...
+                   'gui_LayoutFcn',  [] , ...
+                   'gui_Callback',   []);
+if nargin && ischar(varargin{1})
+    gui_State.gui_Callback = str2func(varargin{1});
+end
+
+if nargout
+    [varargout{1:nargout}] = gui_mainfcn(gui_State, varargin{:});
+else
+    gui_mainfcn(gui_State, varargin{:});
+end
+% End initialization code - DO NOT EDIT
+
+
+% --- Executes just before Sim_GUI is made visible.
+function Sim_GUI_OpeningFcn(hObject, eventdata, handles, varargin)
+% This function has no output args, see OutputFcn.
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+% varargin   command line arguments to Sim_GUI (see VARARGIN)
+
+% Choose default command line output for Sim_GUI
+handles.output = hObject;
+
+set(handles.axes1,'XLim',[0 5])
+set(handles.axes1,'YLim', [0 15])
+
+set(handles.axes2,'XLim',[0 0.1])
+set(handles.axes2,'YLim',[0 2])
+
+set(handles.axes3,'XLim',[0 5])
+    set(handles.axes3,'YLim', [(-25) (45)])
+
+set(handles.axes1,'Box','on')
+set(handles.axes2,'Box','on')
+set(handles.axes3,'Box','on')
+set(handles.axes5,'Box','on')
+set(handles.axes6,'Box','on')
+
+axes(handles.axes1)
+ylabel('Velocity (m/s)')
+axes(handles.axes3)
+ylabel('Wheel Force (N)')
+xlabel('time (sec)')
+axes(handles.axes5)
+ylabel('Value')
+axes(handles.axes6)
+ylabel('Road Incline')
+xlabel('time (sec)')
+
+
+axes(handles.axes2)
+xlabel('Floating point error')
+
+
+% Update handles structure
+guidata(hObject, handles);
+
+% UIWAIT makes Sim_GUI wait for user response (see UIRESUME)
+% uiwait(handles.figure1);
+
+
+% --- Outputs from this function are returned to the command line.
+function varargout = Sim_GUI_OutputFcn(hObject, eventdata, handles) 
+% varargout  cell array for returning output args (see VARARGOUT);
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Get default command line output from handles structure
+varargout{1} = handles.output;
+
+
+
+function SimFilebox_Callback(hObject, eventdata, handles)
+% hObject    handle to SimFilebox (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of SimFilebox as text
+%        str2double(get(hObject,'String')) returns contents of SimFilebox as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function SimFilebox_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to SimFilebox (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+% --- Executes on button press in RunButton.
+function RunButton_Callback(hObject, eventdata, handles)
+% hObject    handle to RunButton (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+
+file = get(handles.SimFilebox,'String');
+
+run(file);
+out = load('output.mat');
+
+
+time = out.output(1,:);
+force = out.output(2,:);
+vel = out.output(3,:);
+rounderror = out.output(4,:);
+roadang = out.output(5,:)*180/pi;
+
+n = length(time);
+framerate = 24;
+t = 0:1/(framerate):time(n);
+
+
+for i = 1:10:(length(time))
+    f = i;
+
+    axes(handles.axes5)
+    ylabel('Value')
+    
+    
+    %velocity plot
+    axes(handles.axes1);
+    plot(time(1:f),vel(1:f))
+    set(handles.axes1,'XLim',[0 time(n)])
+    set(handles.axes1,'YLim', [(min(vel)-1) (max(vel)+1)])
+    ylabel('Velocity (m/s)')
+    drawnow
+    
+    
+    axes(handles.axes2);
+    barh(rounderror(f))
+    set(handles.axes2,'XLim',[0 max(rounderror)])
+    set(handles.axes2,'YLim',[0 2]);
+    xlabel('Floating point error')
+    drawnow
+    
+    axes(handles.axes3);
+    plot(time(1:f),force(1:f))
+    set(handles.axes3,'XLim',[0 time(n)])
+    set(handles.axes3,'YLim', [(min(force)-1) (max(force)+1)])
+    ylabel('Wheel Force (N)')
+    xlabel('time (sec)')
+    drawnow
+    
+    axes(handles.axes6);
+    plot(time(1:f),roadang(1:f))
+    set(handles.axes6,'XLim',[0 time(n)])
+    set(handles.axes6,'YLim', [(min(roadang)-10) (max(roadang)+10)])
+    ylabel('Road Incline')
+    xlabel('time (sec)')
+    drawnow
+end
+    
+
+
+
+function edit3_Callback(hObject, eventdata, handles)
+% hObject    handle to edit3 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of edit3 as text
+%        str2double(get(hObject,'String')) returns contents of edit3 as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function edit3_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to edit3 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+
+function edit4_Callback(hObject, eventdata, handles)
+% hObject    handle to edit4 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of edit4 as text
+%        str2double(get(hObject,'String')) returns contents of edit4 as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function edit4_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to edit4 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+
+function edit5_Callback(hObject, eventdata, handles)
+% hObject    handle to edit5 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of edit5 as text
+%        str2double(get(hObject,'String')) returns contents of edit5 as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function edit5_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to edit5 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+% --- Executes on button press in checkbox1.
+function checkbox1_Callback(hObject, eventdata, handles)
+% hObject    handle to checkbox1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hint: get(hObject,'Value') returns toggle state of checkbox1
